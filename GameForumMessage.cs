168a1544cd23a32d38ebf23c4f2bebc0cb05a0f9
TDeadcoldT <45097173+TDeadcoldT@users.noreply.github.com>
2018-11-16 12:31:55 +0000

    Models

---
+++ GameForumMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace mvcGameReview.Models
+{
+    public class GameForumMessage
+    {
+        [Required]
+        public long Id { get; set; }
+
+        [DisplayName("Message Category Type")]
+        public string Category { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Message Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime MessageDate { get; set; }
+
+        public Boolean Approved { get; set; }
+    }
+}
---
+++ GameForumMessageDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcGameReview.Models
+{
+    public class GameForumMessagesDataContext : DbContext
+    {
+        public DbSet<GameForumMessage> GameForumMessages { get; set; }
+
+        static GameForumMessagesDataContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<GameForumMessagesDataContext>());
+        }
+    }
+}
